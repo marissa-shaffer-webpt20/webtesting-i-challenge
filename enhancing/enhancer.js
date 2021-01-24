3f2972b682ce848f3fbbdb8a690fa07dda75ec8b
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-24 14:41:39 -0600

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,37 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newValue = item.enhancement + 1
+    return { ...item, enhancement: newValue }
+  } else
+  return { ...item};
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    const newValue1 = item.durability - 5
+    return { ...item, durability: newValue1 };
+  }
+  if (item.enhancement === 15) {
+    const newValue2 = item.durability - 10
+    return { ...item, durability: newValue2};
+  }
+  if (item.enhancement > 15) {
+    const newValue3 = item.durability - 10
+    const newValue4 = item.enhancement - 1
+    return { ...item, durability: newValue3, enhancement: newValue4 };
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
+  if (item.enhancement > 0) {
+    return {...item, name: `[+${item.enhancement}] ${item.name}`}
+  }
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,33 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const blade = {name: "Blade", enhancement: 12, durability: 40}
+const spear = {name: "Spear", enhancement: 20, durability: 60}
+const maul = {name: "Maul", enhancement: 15, durability: 40}
+
+test ("testing repair", () => {
+    expect(enhancer.repair(blade).durability).toBe(100)
+})
+
+test('testing success', () => {
+    expect(enhancer.success(blade).enhancement).toBe(13)
+    expect(enhancer.success(spear).enhancement).toBe(20)
+    expect(enhancer.success(maul).enhancement).toBe(16)
+})
+
+test('testing durability failure', () => {
+    expect(enhancer.fail(blade).durability).toBe(35)
+    expect(enhancer.fail(spear).durability).toBe(50)
+    expect(enhancer.fail(maul).durability).toBe(30)
+})
+
+test('testing enhancement failure', () => {
+    expect(enhancer.fail(blade).enhancement).toBe(12)
+    expect(enhancer.fail(spear).enhancement).toBe(19)
+    expect(enhancer.fail(maul).enhancement).toBe(15)
+})
+
+test('testing modified name', () => {
+    expect(enhancer.get(blade).name).toBe("[+12] Blade")
+    expect(enhancer.get(spear).name).toBe("[+20] Spear")
+    expect(enhancer.get(maul).name).toBe("[+15] Maul")
+})
